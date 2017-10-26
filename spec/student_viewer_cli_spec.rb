1b2cc1a78f41b1723e18f133816fc1839a4607ed
Greg Fisher <me@gnfisher.com>
2017-10-26 19:32:53 -0300

    Note

--- spec/student_viewer_cli_spec.rb
+++ spec/student_viewer_cli_spec.rb
@@ -1,5 +1,7 @@
 require "student_viewer_cli"
 
+# Note to self: http://rspec.info/blog/2014/05/notable-changes-in-rspec-3/#new-output-matcher
+# Use output() matcher
 describe StudentViewerCLI do
   context "takes a folder of txt file and outputs student records" do
     it "sorted by campus (ascending) and last_name (ascending)"
