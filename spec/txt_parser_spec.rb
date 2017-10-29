7785556f52e44db65f4e3566bd7eac430383d444
Greg Fisher <me@gnfisher.com>
2017-10-29 09:19:18 -0300

    Clearer spec

--- spec/txt_parser_spec.rb
+++ spec/txt_parser_spec.rb
@@ -2,7 +2,7 @@ require "txt_parser"
 
 describe TXTParser do
   it "separates fields by comma, dollar sign, or pipe chars" do
-    fixture_files do |file_path|
+    fixture_files.each do |file_path|
 
       result = TXTParser.new(file_path).rows.first
 
@@ -10,10 +10,9 @@ describe TXTParser do
     end
   end
 
-  def fixture_files(&block)
+  def fixture_files
     Dir.foreach("spec/fixtures").
       reject { |f| f == "." || f == ".." }.
-      map { |f| "spec/fixtures/#{f}" }.
-      each(&block)
+      map { |f| "spec/fixtures/#{f}" }
   end
 end
